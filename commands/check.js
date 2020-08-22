ec26a18a7705cfe730e7da7eb3bb8ef587d11eec
Noah Sealy <noah.sealy8@gmail.com>
2020-08-22 00:58:18 -0300

    deleted check.js

--- commands/check.js
+++
@@ -1,6 +0,0 @@
-module.exports.run = async (client, message, args) => {
-    console.log(client.loggedIn);
-    if (client.loggedIn.includes(message.author.id)) {
-        console.log(`We got em! ${message.author.id}`);
-    }
-}
